3bf7928812980411ea1ee938cfe840f90d8bd6a7
Ashby Marriott <ashby@stevemarriott.com>
2025-12-06 20:12:24 -0500

    Implemented block_to_block_type() function for block type identification and added some tests.

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     final_blocks = []
@@ -6,3 +16,29 @@ def markdown_to_blocks(markdown):
         if cleaned:
             final_blocks.append(cleaned)
     return final_blocks
+
+def block_to_block_type(block):
+    if block.startswith(('# ', '## ', '### ', '#### ', '##### ', '###### ')):
+        return BlockType.HEADING
+    if block.startswith('```') and block.endswith('```'):
+        return BlockType.CODE
+    block_lines = block.split('\n')
+    starts_with_quote = True
+    starts_with_unordered = True
+    starts_with_ordered = True
+    ordered_count = 1
+    for i in block_lines:
+        if not i.startswith('>'):
+            starts_with_quote = False
+        if not i.startswith('- '):
+            starts_with_unordered = False
+        if not i.startswith(f'{ordered_count}. '):
+            starts_with_ordered = False
+        ordered_count += 1
+    if starts_with_quote:
+        return BlockType.QUOTE
+    if starts_with_unordered:
+        return BlockType.UNORDERED_LIST
+    if starts_with_ordered:
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestMarkdownBlocks(unittest.TestCase):
@@ -47,3 +47,46 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_type_heading1(self):
+        block = """# This is a heading block
+This is the heading continued on the next line"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    
+    def test_block_to_block_type_heading2(self):
+        block = """## This is a heading block
+This is the heading continued on the next line"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_block_to_block_type_heading6(self):
+        block = """###### This is a heading block
+This is the heading continued on the next line"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_block_to_block_type_paragraph1(self):
+        block = """ This is a paragraph block
+This is the heading continued on the next line"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_paragraph2(self):
+        block = """1. This is a paragraph block
+2. That wants to be an ordered list block
+3 . but due to a typo is not"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_ordered_list1(self):
+        block = """1. This is a block
+2. That wants to be an ordered list block
+3. and is!"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+
+
+if __name__ == "__main__":
+    unittest.main()
